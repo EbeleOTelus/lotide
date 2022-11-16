c5b590a453992e53a3790c102313da17895ca312
Ebele <ebele.omanukwue@telus.com>
2022-11-15 19:02:53 -0700

    comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+function eqArrays(array1, array2) {
+  for (i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+
+      return false;
+    }
+
+  }
+  return true;
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
---
+++ reverse.js
@@ -0,0 +1,9 @@
+function reverseString(array){
+  let words = [] ;
+  for (let i=array.length; i >= 0; i--){
+    words += array[i];
+    }
+    return words;
+  }
+    console.log(reverseString("hello goodbye"));
+    console.log(reverseString("1 fish 2 fish"));
