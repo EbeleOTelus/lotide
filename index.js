17357cbae3675a4aa126052f9c65d30f1b91aec6
Ebele <ebele.omanukwue@telus.com>
2022-11-21 17:11:06 -0700

    index.js object

---
+++ index.js
@@ -0,0 +1,33 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqObjects = require('./eqObjects');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  countLetters: countLetters,
+ assertObjectsEqual: assertObjectsEqual,
+ eqObjects: eqObjects,
+ findKey: findKey,
+ findKeyByValue: findKeyByValue,
+letterPositions: letterPositions,
+map: map,
+takeUntil: takeUntil,
+without: without
+};
